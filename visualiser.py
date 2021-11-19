5969bc8ad6bcca31a314397823837c18884e8ba4
PraveerT <praveertowakel@yahoo.com>
2021-11-19 23:21:21 +0400

    changes to visualiser

--- visualiser.py
+++ visualiser.py
@@ -1,14 +1,12 @@
 from numpy import load
 import numpy as np
 import matplotlib.pyplot as plt
-array=load("motions\Hand2.npy")
-array2=load("motions\LSTMdata.npy")
-label=load("motions\labelsHand2.npy")
+OpticalData=load("motions\Optical.npy")
+RadarData=load("motions\Radar.npy")
+label=load("motions\Label.npy")
 
-print (array[10,:,:,:,1].shape)
-# x,y = array2[46,:,:].nonzero()
-x,y,z= array[12,:,:,:,1].nonzero()
-
+print (RadarData[10,:,:,:,1].shape)
+x,y,z= RadarData[12,:,:,:,1].nonzero()
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
